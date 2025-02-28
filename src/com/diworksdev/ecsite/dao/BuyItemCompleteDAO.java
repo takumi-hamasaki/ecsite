9949cb9a2b134ca0e44aa6a291c410a730fa42a3
takumi-hamasaki <hamasakitakumi0830gmail.com>
2025-02-28 22:22:11 +0900

    修正

--- src/com/diworksdev/ecsite/dao/BuyItemCompleteDAO.java
+++ src/com/diworksdev/ecsite/dao/BuyItemCompleteDAO.java
@@ -12,7 +12,8 @@ public class BuyItemCompleteDAO {
 	public void buyItemeInfo(String item_transaction_id, String total_price, String total_count, String
 			user_master_id, String pay) throws SQLException {
 		
-		DBConnector dbConnector = new DBConnector(); Connection connection = dbConnector.getConnection(); 
+		DBConnector dbConnector = new DBConnector(); 
+		Connection connection = dbConnector.getConnection(); 
 		DateUtil dateUtil = new DateUtil();
 		String sql = "INSERT INTO user_buy_item_transaction (item_transaction_id, total_price, total_count, user_master_id, pay, insert_date) VALUES(?, ?, ?, ?, ?, ?)";
 		

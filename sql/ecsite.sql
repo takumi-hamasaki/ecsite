dea7a9861920c5d14bfdccbb6e4f761a1d91ed30
takumi-hamasaki <hamasakitakumi0830gmail.com>
2025-03-01 01:25:15 +0900

    sqlファイルecsite2でやってみる

--- sql/ecsite.sql
+++ sql/ecsite.sql
@@ -1,8 +1,8 @@
 set names utf8;
 set foreign_key_checks = 0;
-drop database if exists ecsite;
-create database if not exists ecsite;
-use ecsite;
+drop database if exists ecsite2;
+create database if not exists ecsite2;
+use ecsite2;
 
 drop table if exists login_user_transaction;
 
